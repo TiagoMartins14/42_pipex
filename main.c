8d2abdbf27dccd3ff055a0c6d7cdc98bc7f76dc1
TiagoMartins14 <92262694+TiagoMartins14@users.noreply.github.com>
2023-10-17 12:54:51 +0100

    In the process of extracting the PATH
    In the process of extracting the PATH environment to be used inside the program

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: patatoss <patatoss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 15:25:50 by tiaferna          #+#    #+#             */
-/*   Updated: 2023/10/15 18:38:20 by patatoss         ###   ########.fr       */
+/*   Updated: 2023/10/17 12:51:34 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,59 +23,84 @@ int	main(int argc, char **argv)
 void	ft_pipex(char **argv)
 {
 	int	fd[2];
-	pipe(fd);
-	fd[0] = open(argv[1], O_RDONLY);
-	if (fd[0] == -1) {
-    perror("open input file");
-    exit(EXIT_FAILURE);
-}
-	fd[1] = open(argv[4], O_WRONLY);
-	if (fd[1] == -1) {
-    perror("open output file");
-    exit(EXIT_FAILURE);
-}
-	int	pid1;
-	int	pid2;
-	// char	**cmd1;
-	// char	**cmd2;
-	char	*cmd1[] = {"grep", "a1", NULL};
-	char	*cmd2[] = {"wc", "-l", NULL};
+	if (pipe(fd) == -1)
+		exit(EXIT_FAILURE);
+		
+	fd[READ_END] = open(argv[1], O_RDONLY);
+	if (fd[READ_END] == -1)
+	{
+    	perror("could not open input file");
+    	exit(EXIT_FAILURE);
+	}
+	
+	fd[WRITE_END] = open(argv[4], O_WRONLY | O_CREAT, 0777);
+	if (fd[WRITE_END] == -1)
+	{
+		perror("could not open or create output file");
+		exit(EXIT_FAILURE);
+	}
 
-	// cmd1 = ft_split(argv[2], ' ');
+	int		fd_temp;
+	int		pid1;
+	int		pid2;
+	char	**cmd1;
+	char	**cmd2;
+		
+	cmd1 = ft_split(argv[2], ' ');
+	cmd2 = ft_split(argv[3], ' ');
 
-	// cmd2 = ft_split(argv[3], ' ');
-
-	ft_printf("Before fork1\n");
 	pid1 = fork();
 	if (pid1 == 0)
 	{
-		dup2(fd[WRITE_END], STDOUT_FILENO);
+		fd_temp = open("temp.txt", O_RDWR | O_CREAT, 0777);
+		dup2(fd[READ_END], STDIN_FILENO);
 		close(fd[READ_END]);
 		close(fd[WRITE_END]);
-		if (execve("/usr/bin/grep", cmd1, NULL) == - 1)
-		{
-			perror("execve pid1");
-        	exit(EXIT_FAILURE);
-		}
+		
+		dup2(fd_temp, STDOUT_FILENO);
+		close(fd_temp);
+		execve("/usr/bin/grep", cmd1, NULL);
+		perror("Failed to execute execve pid1");
+		exit(EXIT_FAILURE);
 	}
-	//waitpid(pid1, NULL, 0);
-	ft_printf("Before fork2\n");
+	waitpid(pid1, NULL, 0);
 	pid2 = fork();
 	if (pid2 == 0)
 	{
-		dup2(fd[READ_END], STDIN_FILENO);
+		fd_temp = open("temp.txt", O_RDWR | O_CREAT, 0777);
+		dup2(fd_temp, STDIN_FILENO);	ft_printf("Before fork2\n");
+		close(fd_temp);
+		dup2(fd[WRITE_END], STDOUT_FILENO);
 		close(fd[WRITE_END]);
 		close(fd[READ_END]);
-		if (execve("/usr/bin/wc", cmd2, NULL) == -1)
-		{
-			perror("execve pid2");
-        	exit(EXIT_FAILURE);
-		}
+		execve("/usr/bin/wc", cmd2, NULL);
+		perror("Failed to execute execve pid2");
+		exit(EXIT_FAILURE);
 	}
 	close(fd[READ_END]);
 	close(fd[WRITE_END]);
-	//waitpid(pid1, NULL, 0);
-	//waitpid(pid2, NULL, 0);
+	close(fd_temp);
+	waitpid(pid2, NULL, 0);
+}
+
+char	*ft_paths_environment()
+{
+	int	fd;
+	int	pid;
+	pid = fork();
+	if (pid == - 1)
+		return (1);
+	if (pid == 0)
+	{
+		fd = open(fd, STDOUT_FILENO);
+		if (fd = -1)
+			return (2);
+		close(fd);
+        char *const cmd[] = {"sh", "-c", "echo $PATH", NULL};
+		execve("/bin/sh", cmd, NULL);
+		perror("Failed to run execve");
+        exit(EXIT_FAILURE);
+	}
 }
 
 char	*ft_check_path(char *all_paths, char* cmd)
